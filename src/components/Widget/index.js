edffc2cccd81a12488e88be0bdc3c4a8786032e6
JonathanGrossmanITC <jonathan@itc.tech>
2021-04-01 22:09:26 +0300

    revert back

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -109,12 +109,11 @@ class Widget extends Component {
   }
 
   getSessionId() {
-    const { storage, customSessionID } = this.props;
+    const { storage } = this.props;
     // Get the local session, check if there is an existing session_id
     const localSession = getLocalSession(storage, SESSION_NAME);
     const localId = localSession ? localSession.session_id : null;
-    // return localId;
-    return customSessionID;
+    return localId;
   }
 
   sendMessage(payload, text = '', when = 'always', tooltipSelector = false) {
