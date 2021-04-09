02e430acfeb0a37d1461176a933a2d8a7f3b2636
JonathanGrossmanITC <jonathan@itc.tech>
2021-04-09 10:59:22 +0300

    updating widget store

--- src/store/reducers/displayMsgIndex.js
+++ src/store/reducers/displayMsgIndex.js
@@ -1,16 +1,16 @@
 import { List, fromJS } from 'immutable';
 import { MESSAGE_SENDER, SESSION_NAME } from 'constants';
 
-// import {
-//   createButtons,
-//   createNewMessage,
-//   createCarousel,
-//   createVideoSnippet,
-//   createImageSnippet,
-//   createComponentMessage,
-//   storeMessageTo,
-//   getLocalSession
-// } from './helper';
+import {
+    createButtons,
+    createNewMessage,
+    createCarousel,
+    createVideoSnippet,
+    createImageSnippet,
+    createComponentMessage,
+    storeMessageTo,
+    getLocalSession,
+} from './helper';
 
 import * as actionTypes from '../actions/actionTypes';
 
@@ -22,12 +22,11 @@ export default function(storage) {
     };
 
     return function reducer(state = initialState, action) {
-        const storeMessage = storeMessageTo(storage);
         switch (action.type) {
             // Each change to the redux store's message list gets recorded to storage
             case actionTypes.CHANGE_DISPLAY_MSG_INDEX: {
                 console.log(state);
-                return storeMessage(state);
+                return state;
             }
             //   case actionTypes.ADD_NEW_RESPONSE_MESSAGE: {
             //     return storeMessage(state.push(createNewMessage(action.text, MESSAGE_SENDER.RESPONSE)));
